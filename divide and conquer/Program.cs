df137a777cc7cccbd5cb18d60bee9c0556ffd54f
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-05-12 18:11:41 +0300

    Sub & Add function, needs a test

--- divide and conquer/Program.cs
+++ divide and conquer/Program.cs
@@ -1,3 +1,121 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿BigInteger x = new BigInteger("1313234242425");
+Console.WriteLine(x);
+BigInteger y = new BigInteger("-23") + new BigInteger("95");
+Console.WriteLine(y);
+BigInteger z = new BigInteger("2405") - new BigInteger("-280");
+Console.WriteLine(z);
 
-Console.WriteLine("Hello, World!");
+
+public class BigInteger
+{
+    private int[] _numbers;
+    private bool _isPositive = true;
+
+    public BigInteger(string value)
+    {
+        if (value[0] == '-')
+        {
+            _isPositive = false;
+            value = value.Substring(1);
+        }
+
+        _numbers = new int[value.Length];
+        for (int i = value.Length - 1; i >= 0; i--)
+        {
+            _numbers[value.Length - 1 - i] = int.Parse(value[i].ToString());
+        }
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        if (!_isPositive)
+        {
+            result += "-";
+        }
+        for (int i = _numbers.Length - 1; i >= 0; i--)
+        {
+            result += _numbers[i].ToString();
+        }
+        return result;
+    }
+
+    public BigInteger Add(BigInteger another)
+    {
+        int[] a = _numbers;
+        int[] b = another._numbers;
+        int[] result = new int[Math.Max(a.Length, b.Length) + 1];
+
+        int carry = 0;
+        if (!_isPositive && another._isPositive)
+        {
+            BigInteger number = new BigInteger(this.ToString().TrimStart('-'));
+            return another.Sub(number);
+        }
+        if (_isPositive && !another._isPositive)
+        {
+            BigInteger number = new BigInteger(another.ToString().TrimStart('-'));
+            return this.Sub(number);
+        }
+        else
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                int sum = carry;
+                if (i < a.Length) sum += a[i];
+                if (i < b.Length) sum += b[i];
+                result[i] = sum % 10;
+                carry = sum / 10;
+            }
+
+            BigInteger bigIntResult = new BigInteger(string.Join("", result.Reverse()).TrimStart('0'));
+            bigIntResult._isPositive = _isPositive;
+            return bigIntResult;
+        }
+    }
+
+
+
+
+    public BigInteger Sub(BigInteger another)
+    {
+        int[] a = _numbers;
+        int[] b = another._numbers;
+        int[] result = new int[Math.Max(a.Length, b.Length)];
+
+        int borrow = 0;
+        if (_isPositive && !another._isPositive)
+        {
+            BigInteger number = new BigInteger(another.ToString().TrimStart('-'));
+            return this.Add(number);
+        }
+        for (int i = 0; i < result.Length; i++)
+        {
+            int diff = borrow;
+            if (i < a.Length) diff += a[i];
+            if (i < b.Length) diff -= b[i];
+            result[i] = (diff + 10) % 10;
+            if (diff < 0)
+            {
+                borrow = -1;
+            }
+            else
+            {
+                borrow = 0;
+            }
+
+        }
+
+        BigInteger bigIntResult = new BigInteger(string.Join("", result.Reverse()).TrimStart('0'));
+        bigIntResult._isPositive = borrow >= 0;
+        return bigIntResult;
+    }
+
+
+
+
+    public static BigInteger operator +(BigInteger a, BigInteger b) => a.Add(b);
+    public static BigInteger operator -(BigInteger a, BigInteger b) => a.Sub(b);
+}
+
+
